dd19be8241227ff95fd6d2caf21e0a19dc2f2f06
actuday <actuday6418@gmail.com>
2021-12-03 13:44:21 +0530

    test

--- src/widgets/paragraph.rs
+++ src/widgets/paragraph.rs
@@ -194,7 +194,7 @@ impl<'a> Widget for Paragraph<'a> {
             }
             let mut file = std::fs::OpenOptions::new().write(true).append(true).open("log.txt").unwrap();
             use std::io::Write;
-            write!(&mut file,"{}",y.to_string()).unwrap();
+            write!(&mut file,"{}@",y.to_string()).unwrap();
         }
     }
 }
